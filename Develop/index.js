533b28d1998038a825747054d75f17b5b9f41f35
keithlyang <keithluckiyang@gmail.com>
2022-01-13 21:34:52 -0600

    added lines 64-74

--- Develop/index.js
+++ Develop/index.js
@@ -9,49 +9,66 @@ inquirer.prompt([
       },
       {
         type: 'input',
-        name: 'descrition',
-        message: 'Write a short desription about your project:',
+        name: 'title',
+        message: 'What is the title of your project?',
       },
-      {
-        type: 'input',
-        name: 'installation',
-        message: 'What installation instruction are there?',
-      },
-      {
-        type: 'input',
-        name: 'usage',
-        message: 'What usage information are there?',
-      },
-      {
-        type: 'input',
-        name: 'contribution',
-        message: 'How many contribution guidelines?',
-      },
-      {
-        type: 'input',
-        name: 'test',
-        message: 'How many test instructions?',
-      },
-      {
-        type: 'input',
-        name: 'github',
-        message: 'Enter your GitHub Username:',
-      },
-      {
-        type: 'input',
-        name: 'email',
-        message: 'What is your email?',
-      },
-      {
-        type: "list",
-        name: "license",
-        message: "license choose please:",
-        choices: ["MIT", "Eclipse", "IMB"],
+    //   {
+    //     type: 'input',
+    //     name: 'descrition',
+    //     message: 'Write a short desription about your project:',
+    //   },
+    //   {
+    //     type: 'input',
+    //     name: 'installation',
+    //     message: 'What installation instruction are there?',
+    //   },
+    //   {
+    //     type: 'input',
+    //     name: 'usage',
+    //     message: 'What usage information are there?',
+    //   },
+    //   {
+    //     type: 'input',
+    //     name: 'contribution',
+    //     message: 'How many contribution guidelines?',
+    //   },
+    //   {
+    //     type: 'input',
+    //     name: 'test',
+    //     message: 'How many test instructions?',
+    //   },
+    //   {
+    //     type: 'input',
+    //     name: 'github',
+    //     message: 'Enter your GitHub Username:',
+    //   },
+    //   {
+    //     type: 'input',
+    //     name: 'email',
+    //     message: 'What is your email?',
+    //   },
+    //   {
+    //     type: "list",
+    //     name: "license",
+    //     message: "license choose please:",
+    //     choices: ["MIT", "Eclipse", "IMB"],
       
-      },
-      {
-        type: 'question',
-        name: 'question',
-        message: 'What kind of question you have?',
-      },
+    //   },
+    //   {
+    //     type: 'question',
+    //     name: 'question',
+    //     message: 'What kind of question you have?',
+    //   },
 ])
+
+.then((answers) => {
+    var readmeContent = generateMD(answers);
+
+    fs.writeFile('testREADME.md', readmeContent , (err) =>
+      err ? console.log(err) : console.log('Successfully created a README.md !')
+    );
+  });
+
+  function generateMD(input) {
+      return `# ${input.title}`
+  }
