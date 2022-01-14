0590ce1674eacba3cd62b1428bb1fcd0b692263b
keithlyang <keithluckiyang@gmail.com>
2022-01-13 22:00:59 -0600

    content to testreadme

--- Develop/index.js
+++ Develop/index.js
@@ -4,7 +4,7 @@ const fs = require('fs');
 inquirer.prompt([
       {
         type: 'input',
-        name: 'name',
+        name: 'username',
         message: 'What is your name?',
       },
       {
@@ -12,53 +12,53 @@ inquirer.prompt([
         name: 'title',
         message: 'What is the title of your project?',
       },
-    //   {
-    //     type: 'input',
-    //     name: 'descrition',
-    //     message: 'Write a short desription about your project:',
-    //   },
-    //   {
-    //     type: 'input',
-    //     name: 'installation',
-    //     message: 'What installation instruction are there?',
-    //   },
-    //   {
-    //     type: 'input',
-    //     name: 'usage',
-    //     message: 'What usage information are there?',
-    //   },
-    //   {
-    //     type: 'input',
-    //     name: 'contribution',
-    //     message: 'How many contribution guidelines?',
-    //   },
-    //   {
-    //     type: 'input',
-    //     name: 'test',
-    //     message: 'How many test instructions?',
-    //   },
-    //   {
-    //     type: 'input',
-    //     name: 'github',
-    //     message: 'Enter your GitHub Username:',
-    //   },
-    //   {
-    //     type: 'input',
-    //     name: 'email',
-    //     message: 'What is your email?',
-    //   },
-    //   {
-    //     type: "list",
-    //     name: "license",
-    //     message: "license choose please:",
-    //     choices: ["MIT", "Eclipse", "IMB"],
+      {
+        type: 'input',
+        name: 'descrition',
+        message: 'Write a short desription about your project:',
+      },
+      {
+        type: 'input',
+        name: 'installation',
+        message: 'What installation instruction are there?',
+      },
+      {
+        type: 'input',
+        name: 'usage',
+        message: 'What usage information are there?',
+      },
+      {
+        type: 'input',
+        name: 'contribution',
+        message: 'How many contribution guidelines?',
+      },
+      {
+        type: 'input',
+        name: 'test',
+        message: 'How many test instructions?',
+      },
+      {
+        type: 'input',
+        name: 'github',
+        message: 'Enter your GitHub Username:',
+      },
+      {
+        type: 'input',
+        name: 'email',
+        message: 'What is your email?',
+      },
+      {
+        type: "list",
+        name: "license",
+        message: "license choose please:",
+        choices: ["MIT", "Eclipse", "IMB"],
       
-    //   },
-    //   {
-    //     type: 'question',
-    //     name: 'question',
-    //     message: 'What kind of question you have?',
-    //   },
+      },
+      {
+        type: 'question',
+        name: 'question',
+        message: 'What kind of question you have?',
+      },
 ])
 
 .then((answers) => {
@@ -70,5 +70,46 @@ inquirer.prompt([
   });
 
   function generateMD(input) {
-      return `# ${input.title}`
-  }
+      return `
+# ${input.title}
+
+# ${input.username}
+      
+## Description
+
+${input.description}
+
+## Installation
+
+${input.installation}
+
+## Usage
+
+${input.usage}
+
+## Contribution
+
+There are ${input.contribution} contribution guidelines.
+
+## Test
+
+There are ${input.text} applications.
+
+## Github
+
+Find me on Github: ${input.github}
+
+## Email
+
+Contact me: ${input.email}
+
+## License
+
+${list.license} License
+
+## Questions
+
+${question.question}
+
+`
+}
